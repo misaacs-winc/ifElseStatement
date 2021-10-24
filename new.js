9d87c77a4a782f747f3c97b7f4c883d26713a23e
misaacs-winc <88142732+misaacs-winc@users.noreply.github.com>
2021-10-24 04:32:40 +0200

    add file via upload

---
+++ new.js
@@ -0,0 +1,24 @@
+const age = 7
+if (age >= 18) {
+    console.log('Welcome to VIP club');
+}
+
+else {
+    console.log('You are too young to be allowed entry, Please come back when above 18 years');
+};
+
+const isFemale = true
+if (isFemale) {
+    console.log('You will get a free entry and a drink if you arrive before 10pm');
+}
+
+else {
+    console.log('The entry fee is $25');
+};
+
+const driverStatus = 'Bob';
+if (driverStatus == 'Bob') {
+    console.log('You aare allowed to drive, please proceed.')
+}
+
+else { 'not a bob and is therefore not allowed to drive' };
